6575ce6d3c5a7e383690ee2a08cb8da152d8ffd1
adrianosouza-dev <adrianobarbosa0503@gmail.com>
2026-05-02 00:41:35 -0300

    feat: adiciona tela de cadastro e estlização

--- loginAPP.py
+++ loginAPP.py
@@ -2,19 +2,49 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+usuarios = {}
+
 @app.route("/")
 def home():
     return render_template("loginHome.html")
 
+
 @app.route("/login", methods=["POST"])
 def login():
-    usuario = request.form["usuario"]
+
+    entrada = request.form["usuario"]   # pode ser user ou email
     senha = request.form["senha"]
 
-    if usuario == "admin" and senha == "1234":
-        return "Login realizado com sucesso!"
-    else:
-        return "Usuário ou senha incorretos"
+    for usuario, dados in usuarios.items():
+
+        if (entrada == usuario or entrada == dados["email"]) \
+           and dados["senha"] == senha:
+
+            return f"Bem-vindo {usuario}"
+
+    return "Usuário ou senha incorretos"
+
+
+@app.route("/cadastro")
+def cadastro():
+    return render_template("cadastroHome.html")
+
+
+@app.route("/cadastrar", methods=["POST"])
+def cadastrar():
+    usuario = request.form["usuario"]
+    email = request.form["email"]
+    senha = request.form["senha"]
+
+    usuarios[usuario] = {
+        "email": email,
+        "senha": senha
+    }
+    
+    print(usuarios)
+
+    return redirect(url_for("home"))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
